f6a62d910f8e442497922a8adbf55fd05bc714c8
masaka9 <hztmj9@gmail.com>
2015-09-13 13:04:24 +0800

    package and test

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/masaka9/golearn/stringutil"
+)
 
 func main() {
 	fmt.Printf("hello, World\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
